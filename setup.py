97bea072e8f2fae29666893000466d0c3030c97b
siddharth17196 <siddharth17196@iiitd.ac.in>
2021-04-09 21:33:38 +0530

    version 0.1.0

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name="elt", version="0.1.0", packages=find_packages())
+setup(
+    name="elt",
+    version="0.1.0",
+    author="Siddharth Nair",
+    description="English to Indian Language Transliteration",
+    packages=find_packages(),
+)
